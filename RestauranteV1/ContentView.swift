91614833b9a68cd07094b6bbceac5f1f351bd9cb
diego <diego.ochoa.maldonado@hotmail.com>
2022-10-22 23:26:56 -0500

    Initial Commit

---
+++ RestauranteV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestauranteV1
+//
+//  Created by Diego OM on 22/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestauranteV1/RestauranteV1App.swift
@@ -0,0 +1,17 @@
+//
+//  RestauranteV1App.swift
+//  RestauranteV1
+//
+//  Created by Diego OM on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RestauranteV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
